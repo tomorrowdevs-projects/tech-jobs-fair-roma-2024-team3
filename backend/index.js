9f98576624f85d736ac1a7535605ebb25c9f21da
Matteo Santoro <matteosantoro1998@gmail.com>
2024-09-16 11:14:52 +0200

    Backend folder e merge attivita

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const attivitaRoutes = require('./routes/attivita'); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 app.use('/auth', authRoutes);
+app.use('/attivita', attivitaRoutes); 
 
 const port = 3001;
 
