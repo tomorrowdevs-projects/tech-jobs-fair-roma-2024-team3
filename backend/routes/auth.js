a0b5f9812d7ae7a526909c5be808a75a52c18aaf
Omar Said Ibrahim <omarsaidita@gmail.com>
2024-09-16 15:06:44 +0200

    added name for signup and return user object on login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,47 +1,45 @@
 // routes/auth.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { validateEmailMiddleware, isExistingEmailMiddleware } = require('../middleware/authMiddleware');
-
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { validateEmailMiddleware, isExistingEmailMiddleware } = require("../middleware/authMiddleware");
 
 // User registration
-router.post('/signup', validateEmailMiddleware,isExistingEmailMiddleware, async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
+router.post("/signup", validateEmailMiddleware, isExistingEmailMiddleware, async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new User({ email, password: hashedPassword });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ error: 'Registration failed' });
-    }
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
+  }
 });
 
 // User login
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(401).json({ error: 'Authentication failed' });
-        }
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Authentication failed' });
-        }
-        const token = jwt.sign({ userId: user._id }, 'hackathon-rome-2024', {
-            expiresIn: '1h',
-        });
-        res.status(200).json({ token });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Login failed' });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
     }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+    const token = jwt.sign({ userId: user._id }, "hackathon-rome-2024", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Login failed" });
+  }
 });
-
 
 module.exports = router;
