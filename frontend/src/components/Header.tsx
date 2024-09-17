4cc4554e46ec44d2aa25853d9f3944890e3e11e7
Omar Said Ibrahim <omarsaidita@gmail.com>
2024-09-17 10:31:43 +0200

    improved code and changed names

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { IoMdLogOut } from "react-icons/io"
+
+interface Props {
+    logout: () => void
+    navigate: (route: string) => void
+    username?: string
+}
+
+const Header = ({ logout, navigate, username }: Props) => {
+    return (
+        <div className="flex justify-between items-center w-full px-4 pt-4">
+            {username && <p className="text-[40px] md:text-[50px] font-semibold">Ciao, {username} &#128075;</p>}
+            <button
+                onClick={() => {
+                    logout()
+                    navigate("/")
+                }}
+                className="text-blue-500 p-1 border-2 border-blue-500 rounded-full"
+            >
+                <IoMdLogOut size={30} />
+            </button>
+        </div>
+    )
+}
+
+export default Header
